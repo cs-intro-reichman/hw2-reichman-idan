dac5df831ce73502997e1a09e29758620bb99e8f
David Reichman <davidreichman@MacBook-Pro-Davidr.local>
2025-11-07 11:31:15 +0200

    completed assignment

--- Collatz.java
+++ Collatz.java
@@ -13,10 +13,10 @@ public class Collatz {
 		int n2 = 0;
 		for (int i = 1; i<= n1; i++) {
 			n2 = i;
-			if (long1) {System.out.print(n2 + " ");}
 			if (i==1) {
 				n2=4;
 			}
+			if (long1) {System.out.print(n2 + " ");}
 			while (n2!=1) {
 				if ( n2%2==0){
 					n2 = n2/2;
